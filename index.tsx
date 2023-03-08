b6555e7293a6eae1930b885d7c20ddf9f33aa791
Daniel Glassford <mbushpilot2b@gmail.com>
2023-03-08 12:10:35 -0500

    Removing Demo Switch

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 const Demo = React.lazy(() => import("./demo"));
-const DemoSwitch = React.lazy(() => import("./demo_switch"));
 import {
   HashRouter,
   Routes,
@@ -14,7 +13,6 @@ const rootElement = window.document.getElementById("root");
 ReactDOM.render((
   <HashRouter>
     <Routes>
-      <Route path="/switch" element={<React.Suspense fallback={<>...</>}><DemoSwitch /></React.Suspense>} />
       <Route path="*" element={<React.Suspense fallback={<>...</>}><Demo /></React.Suspense>} />
     </Routes>
   </HashRouter>
